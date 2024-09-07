fd5c539aee2b7f140dfc581a44130f9b45e147d6
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-07 20:05:34 +0000

    initial commit

---
+++ app/Template/Footer.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Footer Class.
+ *
+ * A simple class for outputting the appropriate footer credit text.
+ *
+ * @package   Workfolio
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/workfolio
+ */
+
+namespace Workfolio\Template;
+use Workfolio\Tools\PoweredBy;
+use Workfolio\Tools\Mod;
+
+/**
+ * Powered by class.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class Footer {
+
+	/**
+	 * Displays a random powered by quote.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public static function displayCredit() {
+
+		echo static::renderCredit();
+	}
+
+	/**
+	 * Returns a random powered by quote.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return string
+	 */
+	public static function renderCredit( array $args = [] ) {
+
+		$args = wp_parse_args( $args, [
+			'before' => '<p class="app-footer__credit">',
+			'after'  => '</p>'
+		] );
+
+		$text = Mod::get( 'theme_footer_powered_by' ) ? PoweredBy::render() : Mod::get( 'theme_footer_custom_credit' );
+
+		return sprintf(
+			'%s%s%s',
+			$args['before'],
+			wp_kses( $text, static::allowedTags() ),
+			$args['after']
+		);
+	}
+
+	/**
+	 * Returns an array of allowed tags in footer text.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return array
+	 */
+	public static function allowedTags() {
+
+		return [
+			'a'       => [ 'href' => true, 'title' => true, 'class' => true ],
+			'abbr'    => [ 'title' => true ],
+			'acronym' => [ 'title' => true ],
+			'bold'    => [ 'class' => true ],
+			'code'    => [ 'class' => true ],
+			'em'      => [ 'class' => true ],
+			'i'       => [ 'class' => true ],
+			'span'    => [ 'class' => true ],
+			'strong'  => [ 'class' => true ],
+			'br'	  => [ 'class' => true ]
+		];
+	}
+}
