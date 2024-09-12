b0ce0d29d3253256b0b71d32f2819dc2932dc836
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-12 04:27:25 +0000

    initial commit

---
+++ config/image-sizes.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Image Sizes Config.
+ *
+ * Defines the image sizes that the theme sets.
+ *
+ * @package   Workfolio
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/workfolio
+ */
+
+ return [
+
+	'workfolio-landscape-medium' => [
+		'label'  => __( 'Landscape: Medium', 'workfolio' ),
+		'width'  => 640,
+		'height' => 360
+	],
+	'workfolio-landscape-large' => [
+		'label'  => __( 'Landscape: Large', 'workfolio' ),
+		'width'  => 896,
+		'height' => 504
+	],
+	'workfolio-landscape-extra-large' => [
+		'label'  => __( 'Landscape: Extra Large', 'workfolio' ),
+		'width'  => 1366,
+		'height' => 768
+	],
+];
